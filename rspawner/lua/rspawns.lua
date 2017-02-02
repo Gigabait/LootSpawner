e99ea3bd00e073f9bc6c81cfaba52976237f0959
Velkon <velkongmod@gmail.com>
2017-02-02 15:48:30 -0700

    meme

---
+++ rspawner/lua/rspawns.lua
@@ -0,0 +1,11 @@
+rspawns.spawners = {} -- Do not remove.
+
+-- You can use these 2 examples here for rp_downtown_v4c_v2
+-- You can look at a entity and type "rspawn" in console to get a premade line!
+-- Make sure to watch the instructional video too.
+
+-- Spawn pistol ammo every 10 seconds.
+-- rspawns.Register(Vector(-1551.3336181641,-348.03533935547,-67.677040100098),"item_ammo_pistol",10)
+
+-- Spawn smg ammo every 5 seconds.
+-- rspawns.Register(Vector(-1618.0537109375,-350.23950195313,-67.466087341309),"item_ammo_smg1",5)
